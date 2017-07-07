66600ebf7b53bb47ba738ec1a6be4f2ea42b2c39
Anuwat Angkuldee <angkuldee_123@hotmail.com>
2017-07-07 19:04:57 +0700

    devStaffProj - 2nd upload

---
+++ app/profile-detail.ts
@@ -0,0 +1,6 @@
+export class ProfileDetail {
+    id: string;
+    name: string;
+    image: string;
+    message: string;
+}
---
+++ app/timeline-element.ts
@@ -0,0 +1,10 @@
+export class TimelineElement {
+    id: string;
+    date: number;
+    month: number;
+    year: number;
+    hour: number;
+    minute: number;
+    image: string;
+    message = Array();
+}
